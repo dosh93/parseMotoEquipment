5d4c7732732e6d78de212e88364d99d4faab830d
dosh <rmatyuk@yandex.ru>
2023-05-09 22:32:28 +0300

    fix start

--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 mkdir -p ./parser_bot/common/
 mkdir -p ./parsers_api/common/
 mkdir -p ./service_update_price/common/
+mkdir -p ./currency_bot/common/
 cp -r ./common/* ./parser_bot/common/
 cp -r ./common/* ./parsers_api/common/
 cp -r ./common/* ./service_update_price/common/
+cp -r ./common/* ./currency_bot/common/
 
 docker-compose down
 docker-compose build

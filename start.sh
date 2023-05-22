7ecc6de9143168a153572d9ba489b3ce73371c7c
dosh <rmatyuk@yandex.ru>
2023-05-22 19:05:24 +0300

    fix docker-compose.yml

--- start.sh
+++ start.sh
@@ -11,14 +11,14 @@ then
   done
   docker-compose down
   docker-compose build
-  docker-compose up -d
+  docker-compose -p parsemotoequipment  up -d
 else
   if [[ " ${SERVICES[@]} " =~ " ${1} " ]]; then
     mkdir -p ./$1/common/
     cp -r ./common/* ./$1/common/
     docker-compose down $1
     docker-compose build $1
-    docker-compose up -d $1
+    docker-compose -p parsemotoequipment up -d $1
   else
     echo "Invalid service name. Please provide one of the following: ${SERVICES[@]}"
   fi

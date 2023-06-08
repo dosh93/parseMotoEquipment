ba703fb0bfdccf9b5b5e30d1f96c4bf4fa2f977e
dosh <rmatyuk@yandex.ru>
2023-06-08 19:55:20 +0300

    fix promo price

--- parsers_api/parsers/marti_motors/helper.py
+++ parsers_api/parsers/marti_motors/helper.py
@@ -214,7 +214,7 @@ def get_price_with_promo(url, id_product_attribute):
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'origin': 'https://www.martimotos.com',
         'referer': url,
-        'cookie': 'PrestaShop-990b6adc0296b50eeb235103a6e5b9d5=a9a9e9e5b04262e6c5e680f55f70067421e9ee9be7817ff227ed67f0ce6264d4%3AP8kvxRe61QEUTuuHUbmvm0PCkuqVNoWmL8VEvVkP62PXyea42hX0eBNBWOmXcqxspWVFgLeVl4Bz1x9g%2FjvLS1d8rxtqYVBybcx2JrfwWj0%2Bw5k%2FwtUjUtJCJj8g6Q36%2BEBxEZYwJNuBSOkj6qavRl%2F%2FjaKLZ8mvdCWitKiOJHfrKZ%2BaKMAWn9cffq%2F74HEc%2Bt7GJopgBNFnfXTcIArYHxfTMHlXNuM0IR3C1SI%2BTQ7ZY1VAmC1Ky3BVQyfkBIN48shR8%2BUcwZ%2FmLHURSNdcEBnM4%2BdMHZ9LqqkIMAv4PI0cAI3cYlsC9KYKnDq7%2FoP7FE7nzYNzid8o%2FDiB0PSoWw%3D%3D; PHPSESSID=41nto9vgp7808o87m0fkpcm9i1',
+        'cookie': 'PHPSESSID=41nto9vgp7808o87m0fkpcm9i1',
         'sec-ch-ua': '"Chromium";v="112", "YaBrowser";v="23", "Not:A-Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',

2ecd5f85385b5dd23fb89ad72d8fc36ca6aa242f
dosh <rmatyuk@yandex.ru>
2023-06-08 18:14:18 +0300

    fix promo price

--- parsers_api/parsers/marti_motors/helper.py
+++ parsers_api/parsers/marti_motors/helper.py
@@ -203,8 +203,6 @@ async def get_price_on_page(browser_handler_instance, rate, markups, url):
 
 
 def get_price_with_promo(url, id_product_attribute):
-    session = requests.Session()
-    get_response = session.get(url)
     headers = {
         'authority': 'www.martimotos.com',
         'accept': 'application/json, text/javascript, */*; q=0.01',
@@ -212,6 +210,7 @@ def get_price_with_promo(url, id_product_attribute):
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'origin': 'https://www.martimotos.com',
         'referer': url,
+        'cookie': '__lglaw=2%2C3%2C4%2C5; PrestaShop-990b6adc0296b50eeb235103a6e5b9d5=a9a9e9e5b04262e6c5e680f55f70067421e9ee9be7817ff227ed67f0ce6264d4%3AP8kvxRe61QEUTuuHUbmvm0PCkuqVNoWmL8VEvVkP62PXyea42hX0eBNBWOmXcqxspWVFgLeVl4Bz1x9g%2FjvLS1d8rxtqYVBybcx2JrfwWj0%2Bw5k%2FwtUjUtJCJj8g6Q36%2BEBxEZYwJNuBSOkj6qavRl%2F%2FjaKLZ8mvdCWitKiOJHfrKZ%2BaKMAWn9cffq%2F74HEc%2Bt7GJopgBNFnfXTcIArYHxfTMHlXNuM0IR3C1SI%2BTQ7ZY1VAmC1Ky3BVQyfkBIN48shR8%2BUcwZ%2FmLHURSNdcEBnM4%2BdMHZ9LqqkIMAv4PI0cAI3cYlsC9KYKnDq7%2FoP7FE7nzYNzid8o%2FDiB0PSoWw%3D%3D; PHPSESSID=41nto9vgp7808o87m0fkpcm9i1',
         'sec-ch-ua': '"Chromium";v="112", "YaBrowser";v="23", "Not:A-Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -229,7 +228,7 @@ def get_price_with_promo(url, id_product_attribute):
         'checked': 'true'
     }
 
-    response = session.post(url, headers=headers, data=data)
+    response = requests.post(url, headers=headers, data=data)
     logger.info("get_price_with_promo response with %s: %s", id_product_attribute, response.text)
 
     if response.status_code == 200:
